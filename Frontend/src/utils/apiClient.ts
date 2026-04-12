92de3848ff6545835e88882a51a5555a87f12f59
kamdem arielle <arielle.kamdem@pether.io>
2026-04-12 17:47:45 +0100

    updated api endpoint base url

--- Frontend/src/utils/apiClient.ts
+++ Frontend/src/utils/apiClient.ts
@@ -9,7 +9,7 @@ const poolData = {
 const userPool = new CognitoUserPool(poolData);
 
 const apiClient = axios.create({
-  baseURL: 'https://ijufbk0l4c.execute-api.us-east-1.amazonaws.com/api/prod',
+  baseURL: 'https://d3sl6qseokrte.cloudfront.net/api/prod',
 });
 
 let isRefreshing = false;
